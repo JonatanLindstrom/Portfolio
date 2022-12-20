18a227d3eb6d46d8a0f3b9d78bf94c940f314075
Jonatan Lindström <git@jonatanlindstrom.se>
2022-12-20 21:38:17 +0100

    Init commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import '@skeletonlabs/skeleton/styles/all.css';
+  import '@skeletonlabs/skeleton/themes/theme-crimson.css';
+  import "../app.postcss";
+
+  import { AppBar, LightSwitch, GradientHeading } from "@skeletonlabs/skeleton"
+</script>
+
+<svelte:head>
+	<title>Jonatan Lindstr&#xf6;m</title>
+</svelte:head>
+
+<AppBar >
+  <svelte:fragment slot="lead">
+    <GradientHeading tag="h1" direction="bg-gradient-to-br" from="from-primary-500" to="to-accent-500">
+      Jonatan Lindström
+    </GradientHeading>
+  </svelte:fragment>
+
+  <svelte:fragment slot="trail">
+    <LightSwitch />
+  </svelte:fragment>
+</AppBar>
+
+<div class="mt-4">
+  <slot />
+</div>
+
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Avatar } from '@skeletonlabs/skeleton';
+</script>
+
+<!-- <div class="grid grid-cols-2 md:grid-cols-4 gap-2 m-8">
+  <Avatar src="/headshot.jpg" class="col-span-1 select-none" rounded="rounded-3xl" width="w-48" />
+  
+  <div class="col-span-1 md:col-span-3">
+    This website is currently under construction. Please check back later.
+  </div>
+</div> -->
+
+<div class="m-8">
+  This website is currently under construction. Please check back later.
+</div>
